850a9aebfd50c959b925e8cc0d6bca92534db669
Witold Mescheryakov <witcold@users.noreply.github.com>
2013-10-17 04:10:50 -0700

    Create vm1_18.dpr

---
+++ vm1_18.dpr
@@ -0,0 +1,15 @@
+program vm1_18;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  UHelp in 'UHelp.pas',
+  UMain in 'UMain.pas',
+  UIO in 'UIO.pas',
+  UTest in 'UTest.pas';
+
+begin
+  Test;
+  //Readln
+end.
